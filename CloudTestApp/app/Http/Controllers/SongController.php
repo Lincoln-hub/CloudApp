aad2b93c07a05024cf2b85305082993c54b33227
Lincoln-hub <72630978+Lincoln-hub@users.noreply.github.com>
2021-01-27 09:50:46 -0700

    Add files via upload

---
+++ CloudTestApp/app/Http/Controllers/SongController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Song;
+
+class SongController extends Controller
+{
+    // a method to create a new song
+    public function create(Request $request)
+    {
+        $artist = $request['artist'];
+        $mysong = $request['song'];
+        $genre = $request['genre'];
+        
+        $song = new Song();
+        $song->artist=$artist;
+        $song->song=$mysong;
+        $song->genre=$genre;
+        
+        $song->save();
+        
+        return redirect()->back();
+        
+    }
+    
+    // a method to show all the songs
+    public function show()
+    {
+        $songs= Song::all();
+        
+        return view('song',['songs'=>$songs]);
+    }
+    
+}
